88848786c412ea0dbaf36d5e618bc26890974cc1
fullstackleo777 <leo@fullstackleo.net>
2025-01-10 06:52:16 -0600

    add autostart script

---
+++ autostart-dev-apps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# autostart-dev-apps.sh - Start app in order on startup
+
+# Start Terminal
+gnome-terminal &
+sleep 2
+
+# Start KeePassXC
+keepassxc &
+sleep 2
+
+# Start Caja file manager
+caja &
+sleep 2
+
+# Start VSCodium
+vscodium &
+sleep 2
+
+# Start ProtonVPN
+protonvpn &
+sleep 2
+
+# Start Brave Browser
+brave &
